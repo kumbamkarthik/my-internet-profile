014d3e3c3dfe2b3e6809cd9cd28bc4f47018398c
Kumbam Karthik <karthik.april11@gmail.com>
2022-03-14 18:51:36 +0530

    Content Updated

---
+++ Index/Assignment_2/app.js
@@ -0,0 +1,21 @@
+const value = document.querySelector("#value");
+const btnDec = document.querySelector("#btn1");
+const btnReset = document.querySelector("#btn2");
+const btnInc = document.querySelector("#btn3");
+let count=0;
+btnDec.addEventListener("click", function(){
+    value.textContent = --count;
+    if(count<0){
+        value.style.color = "red";
+    }
+})
+btnReset.addEventListener("click", function(){
+    value.textContent=0;
+    value.style.color = "black";
+})
+btnInc.addEventListener("click", function(){
+    value.textContent = ++count;
+    if(count>0){
+        value.style.color = "green";
+    }
+})
