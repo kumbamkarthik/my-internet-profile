d40099be9d7444a368c8a033733a6fd9bdead2b9
Kumbam Karthik <karthik.april11@gmail.com>
2022-03-14 23:47:07 +0530

    content updated

---
+++ Index/Assignment_3/app.js
@@ -0,0 +1,24 @@
+function addition(){
+    var x = parseInt(document.casio.value1.value);
+    var y = parseInt(document.casio.value2.value);
+    var z = x+y;
+    document.casio.value3.value=z;
+}
+function subtraction(){
+    var x = parseInt(document.casio.value1.value);
+    var y = parseInt(document.casio.value2.value);
+    var z = x-y;
+    document.casio.value3.value=z;
+}
+function multiplication(){
+    var x = parseInt(document.casio.value1.value);
+    var y = parseInt(document.casio.value2.value);
+    var z = x*y;
+    document.casio.value3.value=z;
+}
+function division(){
+    var x = parseInt(document.casio.value1.value);
+    var y = parseInt(document.casio.value2.value);
+    var z = x/y;
+    document.casio.value3.value=z;
+}
